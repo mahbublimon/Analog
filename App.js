3c8d917aeb77ea3efc8374e695a5b72ac40b0efa
Mahbub Limon <108203739+mahbublimon@users.noreply.github.com>
2024-07-26 17:26:28 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import MainNavigator from './navigation/MainNavigator';
+
+const App = () => <MainNavigator />;
+
+export default App;
